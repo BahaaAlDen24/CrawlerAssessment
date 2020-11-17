ce90c9c80dff01898f9dc6018b32eb150a8ad273
Bahaa Aldeen <Bahaa Aldeen@DESKTOP-5GJDND0>
2020-11-17 12:04:57 +0200

    Initial commit

---
+++ src/main/java/com/example/CrawlerAssessment/CrawlerAssessmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.CrawlerAssessment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrawlerAssessmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrawlerAssessmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/CrawlerAssessment/CrawlerAssessmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CrawlerAssessment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrawlerAssessmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
